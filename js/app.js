d332b485de6aa06ab620f9d01002cf8b0f121634
violetavillamizara <villamizaravioleta@gmail.com>
2023-10-10 13:22:53 -0500

    feat: :art: modules

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// http://127.0.0.9:5020/presentacion
+// http://127.0.0.9:5020/about
+// http://127.0.0.9:5020/resumen
+// http://127.0.0.9:5020/repos
+// http://127.0.0.9:5020/contacto
+
+// Home
+// http://127.0.0.9:5020
