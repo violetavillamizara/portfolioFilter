c8a09202ba24d09b3cb2787903a50f6ca8a33e99
violetavillamizara <villamizaravioleta@gmail.com>
2023-10-12 08:34:23 -0500

    feat: :sparkles: CRUD

--- js/app.js
+++ js/app.js
@@ -1,55 +1,29 @@
-import { getAll, getOne, deleteOne } from "../storage/portafolio"
+import { getAll, getOne, post, deleteOne } from "../storage/portafolio"
 
-const showTable = (data) => {
-    const myData = document.querySelector("#tabla")
-    tabla.forEach((obj) => {
-        tabla.insertAdjacentHTML("beforeend", `
-            <tr>
-                <th>${obj.id}</th>
-                <td>${obj.nombre}</td>
-                <td>${obj.foto}</td>
-                <td><button class="btnPortafolio"></button>
-                <td>
-                    <button data-mod="${obj.id}" class="btnModificar"></button>
-
-                    <button data-del="${obj.id}" class="btnEliminar"></button>
-                </td>
-            </tr>
-        `)
-    })
-}
-
-let myLibro = document.querySelector("#myLibro");
-let myCategoria = document.querySelector("#myCategoria");
-let myAutor = document.querySelector("#myAutor");
-let myData = document.querySelector("#myData");
+const form=document.querySelector("form")
 
 addEventListener("DOMContentLoaded", async(e)=>{
-    let cat = await getAllCat();
-    cat = cat.map(res=> `<option value="${res.id}">${res.nombre}</option>`);
-    myCategoria.insertAdjacentHTML("beforeend", `${cat.join("")}`);
+    const showTable = (data) => {
+        const myData = document.querySelector("#tabla")
+        tabla.forEach((obj) => {
+            tabla.insertAdjacentHTML("beforeend", `
+                <tr>
+                    <th>${obj.id}</th>
+                    <td>${obj.nombre}</td>
+                    <td>${obj.foto}</td>
+                    <td><button class="btnPortafolio"></button>
+                    <td>
+                        <button ="${obj.id}" class=""></button>
+    
+                        <button ="${obj.id}" class=""></button>
+                    </td>
+                </tr>
+            `)
+        })
+    }
+})
 
-    let aut = await getAllAut();
-    aut = aut.map(res=> `<option value="${res.id}">${res.nombre} ${res.apellido}</option>`);
-    myAutor.insertAdjacentHTML("beforeend", `${aut.join("")}`);
 
-    let relLibro = await ObtenerAllData();
-    console.log(relLibro);
-    relLibro = relLibro.map(res=>{
-        return `
-            <tr>
-                <td>${res.id}</td>
-                <td>${res.isbn}</td>
-                <td><img src="${res.imagen}" style="width: 60px;height: 80px; object-fit: fill;"  /></td>
-                <td>${res.titulo}</td>
-                <td>${(res.categoriaId.nombre) ? res.categoriaId.nombre : res.categoriaId.message}</td>
-                <td>${res.autorId.nombre}</td>
-                <td>${res.editorialId}</td>
-                <td>${res.numPaginacion}</td>
-                <td>${res.fecha}</td>
-                <td>${res.estadoId}</td>
-            </tr>
-        `;
-    })
-    myData.insertAdjacentHTML("beforeend", `${relLibro.join("")}`);
+form.addEventListener("submit", async(e)=>{
+    e.preventDefault()
 })
--- storage/portafolio.js
+++ storage/portafolio.js
@@ -15,6 +15,13 @@ export const getOne = async(id)=>{
     return res;
 }
 
+export const post = async ({ obj= {}} = {}) => {
+    config.method = "POST";
+    config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}/portafolio`, config)).json();
+    return (res.id) ? {status: 201, message: "El libro fue creado correctamente", id: res.id} : undefined;
+}
+
 export const deleteOne = async(id)=>{
     config.method="DELETE";
     config.body=JSON.stringify(obj);
